3ed0dc59160b07bf51316a259f65b1447fb98ed2
Richard Lee <richard_875@me.com>
2024-05-03 00:25:07 +1000

    - Feature: Add MediaType and FetchMedia

---
+++ src/services/FetchMedia.ts
@@ -0,0 +1,18 @@
+import MediaType from "src/types/MediaType";
+
+const API_URL =
+  "https://raw.githubusercontent.com/StreamCo/stan-tv-coding-challenge/master/reactjs/data.json";
+
+const FetchMedia = async (): Promise<MediaType[] | undefined> => {
+  try {
+    const response = await fetch(API_URL);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    window.location.href = "/error";
+  }
+
+  return undefined;
+};
+
+export default FetchMedia;
---
+++ src/types/MediaType.ts
@@ -0,0 +1,13 @@
+type MediaType = {
+  id: number;
+  title: string;
+  description: string;
+  type: string;
+  image: string;
+  rating: string;
+  genre: string;
+  year: number;
+  language: string;
+};
+
+export default MediaType;
