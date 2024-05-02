693913194fa57c6ec8e2243f13911882ec770dd0
Richard Lee <richard_875@me.com>
2024-05-03 00:44:32 +1000

    - Feature: Add HomeSkeleton component

---
+++ src/components/HomeSkeleton.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const HomeSkeleton = () => (
+  <Container>
+    {[...Array(20)].map((e, i) => (
+      <Card key={i} />
+    ))}
+  </Container>
+);
+
+export default HomeSkeleton;
+
+const Container = styled.div`
+  gap: 10px;
+  display: flex;
+  overflow: hidden;
+`;
+
+const Card = styled.div`
+  width: 200px;
+  height: 300px;
+  flex-shrink: 0;
+  background-color: #606060;
+`;
